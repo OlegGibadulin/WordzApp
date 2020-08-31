b2a0aeddf8c47b8cacc03968a6f95119a950a497
Timonin Anton <dotruger.37@gmail.com>
2020-08-31 21:53:31 +0300

    Add Body Part category

--- WordzApp/Storage/Storage.swift
+++ WordzApp/Storage/Storage.swift
@@ -31,6 +31,7 @@ struct Storage {
         CategoryStorage(title: "Архитектура", firstColor: #colorLiteral(red: 0.9215686275, green: 0.3725490196, blue: 0.2431372549, alpha: 1), secondColor: #colorLiteral(red: 0.8901960784, green: 0.6078431373, blue: 0.2392156863, alpha: 1), sentences: Architecture),
         CategoryStorage(title: "Эмоции", firstColor: #colorLiteral(red: 0.7803921569, green: 0.2156862745, blue: 0.9647058824, alpha: 1), secondColor: #colorLiteral(red: 0.5490196078, green: 0.2274509804, blue: 0.8980392157, alpha: 1), sentences: Emotion),
         CategoryStorage(title: "Дом", firstColor: #colorLiteral(red: 0.9333333333, green: 0.5254901961, blue: 0.4078431373, alpha: 1), secondColor: #colorLiteral(red: 0.9490196078, green: 0.7176470588, blue: 0.5176470588, alpha: 1), sentences: House),
+        CategoryStorage(title: "Части тела", firstColor: #colorLiteral(red: 0.8880360723, green: 0.9051850438, blue: 0.3957586884, alpha: 1), secondColor: #colorLiteral(red: 0.464773953, green: 0.9658891559, blue: 0.3734448254, alpha: 1), sentences: BodyParts)
     ]
     
     let levelsTitle = [
