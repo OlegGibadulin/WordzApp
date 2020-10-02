fd6281ad6141610f48029d8b6b24b0cca6ebff6c
Timonin Anton <dotruger.37@gmail.com>
2020-10-02 22:49:35 +0300

    Add release 1.1v

--- WordzApp/SceneDelegate.swift
+++ WordzApp/SceneDelegate.swift
@@ -50,10 +50,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
